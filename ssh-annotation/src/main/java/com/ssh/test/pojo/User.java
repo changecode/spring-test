19d3831809b53a69cece3ad440e4ec2bd3fe82b9
Tim <c779333@163.com>
2017-06-09 11:39:06 +0800

    ssh框架搭建简单示例 2017.6.9

---
+++ ssh-annotation/src/main/java/com/ssh/test/pojo/User.java
@@ -0,0 +1,37 @@
+package com.ssh.test.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_user")
+public class User {
+
+	private Integer id;
+	private String name;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
